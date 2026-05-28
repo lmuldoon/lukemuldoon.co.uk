cd3f695f6c0e9bfde739dfe030835fb034d05448
Luke Muldoon <LukeMuldoon@core-marketing.co.uk>
2026-05-28 15:28:10 +0100

    Update

--- wp-content/themes/lukemuldoon/single.php
+++ wp-content/themes/lukemuldoon/single.php
@@ -11,7 +11,7 @@
             <time class="insight-post__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>"><?php echo esc_html(get_the_date('j F Y')); ?></time>
 
         <?php if (has_post_thumbnail()) : ?>
-                <div class="insight-post__featured-image ratio ratio--16-9 mt-12">
+                <div class="insight-post__featured-image ratio ratio--16-9 mt-6 md:mt-12">
                     <?php the_post_thumbnail('full', ['loading' => 'eager', 'class' => 'insight-post__featured-img ratio__content']); ?>
                 </div>
             </div>
